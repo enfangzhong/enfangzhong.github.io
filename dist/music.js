f3af7dd67886381a569cbbbac225fc434dd5bb3e
Enfang <944727327@qq.com>
2019-05-11 18:50:12 +0800

    我又更新了(〃'▽'〃)

--- dist/music.js
+++ dist/music.js
@@ -4,10 +4,10 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
 	{
-        name: '惊蛰',
-        artist: '音阙诗听/王梓钰',
-        url: 'http://www.ytmp3.cn/down/48755.mp3',
-        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+        name: "Dream It Possible-Delacey",
+        artist: 'Brian Crain',
+        url: 'https://music.163.com/song?id=27566697&userid=397350552',
+        cover: 'http://p2.music.126.net/PNRvAXY7IeXMtHN8gE5n4Q==/6665239488419620.jpg?param=130y130',
       },
 	  {
         name: '惊蛰',
