53de2e2a417f719e825a39a4596fbc628a9b0df9
Enfang <944727327@qq.com>
2019-05-11 18:56:56 +0800

    我又更新了(〃'▽'〃)

--- dist/music.js
+++ dist/music.js
@@ -22,10 +22,10 @@ const ap = new APlayer({
         cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
       },
       {
-        name: '惊蛰',
-        artist: '音阙诗听/王梓钰',
-        url: 'http://www.ytmp3.cn/down/48755.mp3',
-        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+        name: '这些民谣 - 一次听个够',
+        artist: '翁大涵',
+        url: 'http://www.ytmp3.cn/down/60222.mp3',
+        cover: 'http://img.ytmp3.cn/image/10.jpg?param=130y130',
       }
     ]
 });
