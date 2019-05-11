d2536625e484d151cd36663e842c487655779cc9
Enfang <944727327@qq.com>
2019-05-11 19:01:59 +0800

    我又更新了(〃'▽'〃)

--- dist/music.js
+++ dist/music.js
@@ -4,10 +4,10 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
 	{
-        name: "Dream It Possible-Delacey",
-        artist: 'Brian Crain',
-        url: 'http://www.ytmp3.cn/down/59129.mp3',
-        cover: 'http://p2.music.126.net/PNRvAXY7IeXMtHN8gE5n4Q==/6665239488419620.jpg?param=130y130',
+        name: "平凡之路",
+        artist: '朴树',
+        url: 'http://www.ytmp3.cn/down/59211.mp3',
+        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
       },
 	  {
         name: '惊蛰',
